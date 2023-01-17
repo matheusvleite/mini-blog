919c655f866e1f035296976fec3b52d3e8bda184
Matheus Victor <matheusvictor2000@gmail.com>
2023-01-17 19:06:02 -0300

    CSS do  navbar e footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 import styles from './Footer.module.css';
 const Footer = () => {
     return (
-        <footer>
-            <h3>Escreva sobre o que você tem interesse!</h3>
+        <footer className={styles.footer}>
+            <h3 className={styles.footer_title}>Escreva sobre o que você tem interesse!</h3>
             <p>Mini blog &copy; 2023</p>
         </footer>
     )
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,11 +4,15 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav>
-      <NavLink to="/">Mini <span>Blog</span></NavLink>
-      <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/about">Sobre</NavLink></li>
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={styles.brand}>Mini <span>Blog</span></NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : "")}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className={({ isActive }) => (isActive ? styles.active : "")}>Sobre</NavLink>
+        </li>
       </ul>
     </nav>
   )
