715144a49de60d82af631336a7aa68e8c3741e6d
Matheus Victor <matheusvictor2000@gmail.com>
2023-01-18 13:34:15 -0300

    Criando hook para enviar o post para o banco e enviar atraves da pagina Criar post

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -3,6 +3,7 @@ import styles from './CreatePost.module.css';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuthValue } from '../../context/AuthContext';
+import { useInsertDocument } from '../../hooks/useInsertDocument';
 
 const CreatePost = () => {
     const [title, setTitle] = useState('');
@@ -11,8 +12,22 @@ const CreatePost = () => {
     const [tags, seTags] = useState([]);
     const [formError, setFormError] = useState('');
 
+    const { user } = useAuthValue();
+
+    const { insertDocument, response } = useInsertDocument('posts');
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        setFormError('');
+
+        insertDocument({
+            title,
+            image,
+            body,
+            tags,
+            uid: user.uid,
+            createdBy: user.displayName,
+        });
     };
 
     return (
@@ -64,11 +79,10 @@ const CreatePost = () => {
                         value={tags}
                     />
                 </label>
-                <button className='btn'>Cadastrar</button>
-                {/* {loading ? <button className='btn' disabled>Aguarde...</button> :
-                    <button className='btn'>Cadastrar</button>
+                {response.loading ? <button className='btn' disabled>Aguarde...</button> :
+                    <button className='btn'>Criar post</button>
                 }
-                {error && <p className='error'>{error}</p>} */}
+                {response.error && <p className='error'>{response.error}</p>}
             </form>
         </div>
     )
