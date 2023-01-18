da01a6383e91f5b67e3d9b61de6af916474bb2e7
Matheus Victor <matheusvictor2000@gmail.com>
2023-01-18 13:51:50 -0300

    Validação dos dados do post

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -16,18 +16,36 @@ const CreatePost = () => {
 
     const { insertDocument, response } = useInsertDocument('posts');
 
+    const navigate = useNavigate();
+
     const handleSubmit = (e) => {
         e.preventDefault();
         setFormError('');
+
+        try {
+            new URL(image);
+        } catch (error) {
+            setFormError('A imagem precisa ser uma URL.')
+        }
+
+        const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+        if (!title || !image || !tags || !body) {
+            setFormError("Por favor, preencha todos os campos!")
+        }
+
+        if (formError) return;
 
         insertDocument({
             title,
             image,
             body,
-            tags,
+            tags: tagsArray,
             uid: user.uid,
             createdBy: user.displayName,
         });
+
+        navigate('/')
     };
 
     return (
@@ -83,6 +101,7 @@ const CreatePost = () => {
                     <button className='btn'>Criar post</button>
                 }
                 {response.error && <p className='error'>{response.error}</p>}
+                {formError && <p className='error'>{formError}</p>}
             </form>
         </div>
     )
