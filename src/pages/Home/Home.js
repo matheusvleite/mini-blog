c3dd66c833211356aabbf1c495861a4e396e6d32
Matheus Victor <matheusvictor2000@gmail.com>
2023-01-18 15:54:35 -0300

    Exibindo posts na home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,10 +2,12 @@ import React from 'react';
 import styles from './Home.module.css';
 import { useNavigate, Link } from 'react-router-dom';
 import { useState } from 'react';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import PostDetail from '../../components/PostDetail/PostDetail';
 
 const Home = () => {
     const [query, setQuery] = useState('');
-    const [posts] = useState([]);
+    const { documents: posts, loading } = useFetchDocuments("posts")
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -22,7 +24,10 @@ const Home = () => {
                 />
                 <button className="btn btn-dark">Pesquisar</button>
             </form>
-            <h1>Posts...</h1>
+            {loading && <p>Carregando...</p>}
+            {posts && posts.map(post => (
+                <PostDetail key={post.id} post={post}/>
+            ))}
             {posts && posts.length === 0 && (
                 <div className={styles.noposts}>
                     <p>Não foram encontrados posts</p>
