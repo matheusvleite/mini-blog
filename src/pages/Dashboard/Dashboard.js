15d5f1fcc637601e333209970e79257dcc29d4de
Matheus Victor <matheusvictor2000@gmail.com>
2023-01-19 11:26:53 -0300

    Estruturando o dashboard e mostrando os posts de cada usuario no dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import styles from './Dashboard.module.css';
+import { useAuthValue } from '../../context/AuthContext';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
+    const { user } = useAuthValue();
+    const uid = user.uid
+
+    const { documents: posts } = useFetchDocuments("posts", null, uid);
+
+    const deleteDocument = (id) => {
+
+    }
+
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className={styles.dashboard}>
+            <h2>Dashboard</h2>
+            <p>Gerencie os seus posts</p>
+            {posts && posts.length === 0 ? (
+                <div className={styles.noposts}>
+                    <p>Não foram encontrados posts</p>
+                    <Link to="/posts/create" className='btn'>Criar primeiro post</Link>
+                </div>
+            ) : (
+                <>
+                    <div className={styles.post_header}>
+                        <span>Título</span>
+                        <span>Ações</span>
+                    </div>
+                    {posts && posts.map(post => (
+                        <div key={post.id} className={styles.post_row}>
+                            <p>{post.title}</p>
+                            <div>
+                                <Link to={`/posts/${post.id}`} className='btn btn-outline'>Ver</Link>
+                                <Link to={`/posts/edit/${post.id}`} className='btn btn-outline'>Editar</Link>
+                                <button onClick={() => deleteDocument(post.id)} className='btn btn-outline btn-danger'>Excluir</button>
+                            </div>
+                        </div>
+                    ))}
+                </>
+            )}
         </div>
     )
 }
